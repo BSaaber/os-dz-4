a88e7d7441d8ddd1b49885e2fcb644d2786ad740
Max <66118806+BSaaber@users.noreply.github.com>
2023-02-10 23:19:17 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+const int size = 4096;
+int main() {
+    int fd;
+    ssize_t read_bytes;
+    char buffer[size];
+    if((fd = open("main.c", O_RDONLY)) < 0) {
+        printf("Can\'t open file\n");
+        exit(-1);
+    }
+    read_bytes = read(fd, buffer, size);
+
+    if (read_bytes == -1) {
+        printf("Can\'t read this file\n");
+    }
+    if (close(fd) < 0) {
+        printf("Can\'t close this file\n");
+    }
+    if((fd = open("output.txt", O_WRONLY | O_CREAT, 0666)) < 0) {
+        printf("Can\'t open file\n");
+        exit(-1);
+    }
+    write(fd, buffer, read_bytes);
+    if (close(fd) < 0) {
+        printf("Can\'t close this file\n");
+    }
+    return 0;
+}
